167fdf55c888a54b8cfd2b2366d0bb9e36d10979
Wagner Braga <wfbraga@hotmail.com>
2022-06-04 10:00:56 -0400

    creating model Games and its tests Also adjusting new test on system requiriment to validates assosiation with games and it dependent destroy restriction with errors

---
+++ spec/factories/games.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :game do
+    mode { %i[pvp pve both].sample }
+    resease_date { '2022-06-04 09:22:57' }
+    developer { Faker::Company.name }
+    system_requirement
+  end
+end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,9 @@ FactoryBot.define do
   factory :product do
     sequence(:name) { |n| "Category #{n}" }
     description { Faker::Lorem.paragraph }
-    price { Faker::Commer.price(rage: 100.0..400.0) }
-    productable { nil }
+    price { Faker::Commerce.price(range: 100.0..400.0) }
+    after :build do |product|
+      product.productable = (create(:game))
+    end
   end
 end
